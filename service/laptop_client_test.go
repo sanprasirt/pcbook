6e5be13e300f523769209a2811cf5f864adf9bfe
Sanprasirt boonma <kuforce@gmail.com>
2020-02-16 16:18:41 +0700

    add search laptop services

---
+++ service/laptop_client_test.go
@@ -0,0 +1,141 @@
+package service_test
+
+import (
+	"context"
+	"io"
+	"net"
+	"testing"
+
+	"github.com/sanprasirt/pcbook/serializer"
+
+	"github.com/sanprasirt/pcbook/sample"
+
+	"github.com/sanprasirt/pcbook/pb"
+	"github.com/sanprasirt/pcbook/service"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestClientCreateLaptop(t *testing.T) {
+	t.Parallel()
+
+	laptopStore := service.NewInMemoryLaptopStore()
+	serverAddress := startTestLaptopServer(t, laptopStore)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	laptop := sample.NewLaptop()
+	expectedID := laptop.Id
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Equal(t, expectedID, res.Id)
+
+	// check that the laptop is saved to the store
+	other, err := laptopStore.Find(res.Id)
+	require.NoError(t, err)
+	require.NotNil(t, other)
+
+	requireSampleLaptop(t, laptop, other)
+}
+
+func startTestLaptopServer(t *testing.T, laptopStore service.LaptopStore) string {
+	laptopServer := service.NewLaptopServer(laptopStore)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+
+	listener, err := net.Listen("tcp", ":0") // random available port
+	require.NoError(t, err)
+
+	go grpcServer.Serve(listener)
+
+	return listener.Addr().String()
+}
+
+func newTestLaptopClient(t *testing.T, serverAddress string) pb.LaptopServiceClient {
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
+	require.NoError(t, err)
+	return pb.NewLaptopServiceClient(conn)
+}
+
+func requireSampleLaptop(t *testing.T, laptop1 *pb.Laptop, laptop2 *pb.Laptop) {
+	json1, err := serializer.ProtobufToJSON(laptop1)
+	require.NoError(t, err)
+
+	json2, err := serializer.ProtobufToJSON(laptop2)
+	require.NoError(t, err)
+
+	require.Equal(t, json1, json2)
+}
+
+func TestClientSearchLaptop(t *testing.T) {
+	t.Parallel()
+	filter := &pb.Filter{
+		MaxPriceUsd: 2000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.2,
+		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+
+	laptopStore := service.NewInMemoryLaptopStore()
+	expectedIDs := make(map[string]bool)
+
+	for i := 0; i < 6; i++ {
+		laptop := sample.NewLaptop()
+
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Memery = &pb.Memory{Value: 4096, Unit: pb.Memory_MEGABYTE}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = laptop.Cpu.MinGhz + 2.0
+			laptop.Memery = &pb.Memory{Value: 16, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 6
+			laptop.Cpu.MinGhz = 2.8
+			laptop.Cpu.MaxGhz = laptop.Cpu.MinGhz + 2.0
+			laptop.Memery = &pb.Memory{Value: 64, Unit: pb.Memory_GIGABYTE}
+			expectedIDs[laptop.Id] = true
+		}
+
+		err := laptopStore.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	serverAddress := startTestLaptopServer(t, laptopStore)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+
+	found := 0
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		require.Contains(t, expectedIDs, res.GetLaptop().GetId())
+
+		found += 1
+
+	}
+
+	require.Equal(t, len(expectedIDs), found)
+
+}
---
+++ service/laptop_server.go
@@ -0,0 +1,96 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/sanprasirt/pcbook/pb"
+)
+
+// LaptopServer is the server that provides laptop services
+type LaptopServer struct {
+	laptopStore LaptopStore
+}
+
+// NewLaptopServer returns a new LaptopServer
+func NewLaptopServer(laptopStore LaptopStore) *LaptopServer {
+	return &LaptopServer{laptopStore}
+}
+
+// CreateLaptop is a unary RPC to create a new laptop
+func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+	laptop := req.GetLaptop()
+	log.Printf("receive a create-laptop request with id: %s", laptop.Id)
+
+	if len(laptop.Id) > 0 {
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop ID is not a valid UUID: %v", err)
+		}
+	} else {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "cannot generate a new laptop ID: %v", err)
+		}
+		laptop.Id = id.String()
+	}
+
+	// some heavy processing
+	// time.Sleep(6 * time.Second)
+
+	if ctx.Err() == context.Canceled {
+		log.Print("request is canceled")
+		return nil, status.Error(codes.Canceled, "request is canceled")
+	}
+	if ctx.Err() == context.DeadlineExceeded {
+		log.Print("deadline is exceeded")
+		return nil, status.Error(codes.DeadlineExceeded, "deadline is exceeded")
+	}
+	// save laptop to store
+	err := server.laptopStore.Save(laptop)
+	if err != nil {
+		code := codes.Internal
+		if errors.Is(err, ErrAlreadyExists) {
+			code = codes.AlreadyExists
+		}
+		return nil, status.Errorf(code, "cannot save laptop to the store %v", err)
+	}
+
+	log.Printf("saved laptop with id: %s", laptop.Id)
+
+	res := &pb.CreateLaptopResponse{
+		Id: laptop.Id,
+	}
+
+	return res, nil
+}
+
+// SearchLaptop is a server-streaming RPC to search for laptop
+func (server *LaptopServer) SearchLaptop(req *pb.SearchLaptopRequest, stream pb.LaptopService_SearchLaptopServer) error {
+	filter := req.GetFilter()
+	log.Printf("recieve a search-laptop request with filter: %v", filter)
+
+	err := server.laptopStore.Search(
+		stream.Context(),
+		filter,
+		func(laptop *pb.Laptop) error {
+			res := &pb.SearchLaptopResponse{Laptop: laptop}
+			err := stream.Send(res)
+			if err != nil {
+				return err
+			}
+
+			log.Printf("send laptop with id: %s", laptop.GetId())
+			return nil
+		},
+	)
+	if err != nil {
+		return status.Errorf(codes.Internal, "unexpected error: %v", err)
+	}
+	return nil
+}
